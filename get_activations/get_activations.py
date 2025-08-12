64a9e8d29d22b2a36af8d56c2313a96368e9b377
keimoriyama <keischwiiz@gmail.com>
2025-08-12 14:49:43 +0900

    update path

--- get_activations/get_activations.py
+++ get_activations/get_activations.py
@@ -3,24 +3,19 @@ import argparse
 import pickle
 import sys
 
-sys.path.append("../")
+sys.path.append("./")
 
 import numpy as np
 import pyvene as pv
 import torch
 from datasets import load_dataset
 from tqdm import tqdm
-from transformers import AutoModel, AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from interveners import Collector, ITI_Intervener, wrapper
-
+from interveners import Collector, wrapper
 # Specific pyvene imports
-from utils import (
-    get_llama_activations_pyvene,
-    tokenized_tqa,
-    tokenized_tqa_gen,
-    tokenized_tqa_gen_end_q,
-)
+from utils import (get_llama_activations_pyvene, tokenized_tqa,
+                   tokenized_tqa_gen, tokenized_tqa_gen_end_q)
 
 HF_NAMES = {
     # 'llama_7B': 'baffo32/decapoda-research-llama-7B-hf',
